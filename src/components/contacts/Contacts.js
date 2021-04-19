3cea923262290bcd9cc9f51eed253fb42d77457f
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2021-04-19 06:22:12 +0530

    Search and Filtering options completed

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -24,17 +24,64 @@ const Contacts = () => {
       dispatch({ type: 'SET_CONTACTS', payload });
     };
     getContacts();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const selectMenuHandler = async (e) => {
+    const config = {
+      headers: {
+        Authorization: 'Bearer ' + getToken(),
+      },
+    };
+    const order = e.target.value;
+    const res = await api.get(`/contact/contacts?sort=name:${order}`, config);
+    const { results: payload } = res?.data;
+    console.log(payload);
+    dispatch({ type: 'SET_CONTACTS', payload });
+  };
+
+  const searchHandler = async (e) => {
+    console.log(e.target.value);
+    const result = contacts.filter((contact) =>
+      contact.name.includes(e.target.value)
+    );
+    console.log('result', result);
+    dispatch({ type: 'SET_CONTACTS', payload: result });
+    const config = {
+      headers: {
+        Authorization: 'Bearer ' + getToken(),
+      },
+    };
+    if (e.target.value.length === 0) {
+      const res = await api.get('/contact/contacts', config);
+      const { results: payload } = res?.data;
+      dispatch({ type: 'SET_CONTACTS', payload });
+    }
+  };
 
   return (
     <div className='container'>
-      <Button
-        className='btn btn-info float-right mb-2'
-        onClick={() => history.push(ROUTES.ADDCONTACT)}
-      >
-        Create Contact
-      </Button>
+      <div>
+        <input type='text' name='search' onChange={searchHandler} />
+        <select
+          class='form-select form-select-md'
+          aria-label='Default select example'
+          className='w-auto'
+          onChange={selectMenuHandler}
+          title='Sort Contact number'
+        >
+          <option selected>Sort Contact Number</option>
+          <option value='asc'>Ascending order</option>
+          <option value='desc'>Descending order</option>
+        </select>
+        <Button
+          className='btn btn-info float-right mb-2'
+          onClick={() => history.push(ROUTES.ADDCONTACT)}
+        >
+          Create Contact
+        </Button>
+      </div>
+
       <table className='table shadow'>
         <thead className='bg-danger text-white'>
           <tr>
